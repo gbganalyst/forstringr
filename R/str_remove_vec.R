a812c524343d1e46451e19ffa3b9fce268dda4cd
akins11 <akinwandeayomide24@gmail.com>
2022-08-27 14:05:51 +0100

    proposed string remove vector pattern

---
+++ R/str_remove_vec.R
@@ -0,0 +1,26 @@
+# str_remove_vec is not a propose name, just for example
+
+str_remove_vec <- function(string, patterns) {
+  esc_punt <- c("?", "$", "(", ")", "+", ".", "^", "*", "|", "[", "_", "s", "\\")
+
+  if (any(patterns %in% esc_punt)) {
+    # extract all patterns in esc_punt
+    pt <- patterns[patterns %in% esc_punt]
+
+    # add \\ to all values in pt
+    pt <- str_glue("\\{pt}")
+
+    # combine pt with other patterns
+    patterns <- c(patterns[!patterns %in% esc_punt], pt)
+  }
+
+  # join all patterns supplied as a vector and seperate them with `|`
+  # e.g c('$', '%') = '\\$|%'
+  regex_pattern <- paste0(patterns, collapse = "|")
+
+  str_remove_all(string = string, pattern = regex_pattern)
+}
+
+# str_remove_vec(comorbidity, c("[", "]", ":"))
+#
+# str_remove_vec(comorbidity, c("[", "]", ":"))
