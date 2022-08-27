e2697b5cdcb2eb4c667f7e1ffb243aecd20e95a3
akins11 <akinwandeayomide24@gmail.com>
2022-08-27 14:04:25 +0100

    second string extract request
    - multiple pattern as a vector.

--- R/str_extract_part.R
+++ R/str_extract_part.R
@@ -1,28 +1,31 @@
-#' Extract strings before or after a particular pattern.
+#' Extract strings before or after a particular pattern
 #'
+#' @description
 #' @param string A character vector.
-#' @param part The string position to extract. must be 'before' or 'after'.
-#' @param pattern Pattern to use as boundary.
+#' @param before The position in the string to extract from. If TRUE, the extract
+#' will occur before the pattern; if FALSE, it will happen after the pattern.
+#' @param pattern pattern Pattern to look for.
 #'
 #' @return A subset of the input vector.
 #' @export
 #'
 #' @examples
-#' weekdays <- c("Today_Monday", "Today_Tuesday", "Today_Wednesday", "Today_Thursday",
-#'  "Today_Friday", "Today_Saturday", "Today_Sunday")
+#' weekdays <- c("Monday_1", "Tuesday_2", "Wednesday_3", "Thursday_4",
+#'  "Friday_5", "Saturday_6", "Sunday_7")
 #' str_extract_part(weekdays, "after", "_")
 #'
-#' # Convert to Number
-#' str_extract_part(c("$159", "$587", "$897"), "after", "$") |>
+#' # Convert to Numeric
+#' str_extract_part(c("$159", "$587", "$897"), "after", "$") %>%
 #'  as.numeric()
-str_extract_part <- function(string, part, pattern) {
+#'
+str_extract_part <- function(string, before, pattern) {
   part <- match.arg(part, c("before", "after"))
 
   if (missing(pattern)) {
     stop("argument `pattern` is missing ")
   }
 
-  esc_punt <- c("?", "$", "(", ")", "+", ".", "^", "*", "|")
+  esc_punt <- c("?", "$", "(", ")", "+", ".", "^", "*", "|", "[", "_", "s", "\\")
 
   if (pattern %in% esc_punt) {
     pattern <- stringr::str_glue("\\{pattern}")
@@ -30,12 +33,12 @@ str_extract_part <- function(string, part, pattern) {
 
   tryCatch(
     expr = {
-      if (part == "before") {
+      if (before == TRUE) {
         regex_pattern <- stringr::str_glue("^.*(?={pattern})")
 
         stringr::str_extract(string = string, pattern = regex_pattern)
 
-      } else if (part == "after") {
+      } else if (before == FALSE) {
         regex_pattern <- stringr::str_glue("(?<={pattern}).*$")
 
         stringr::str_extract(string = string, pattern = regex_pattern)
@@ -45,3 +48,65 @@ str_extract_part <- function(string, part, pattern) {
     error = \(e) stop(stringr::str_glue("can't handle {pattern} pattern"))
   )
 }
+
+
+# Multiple pattern as a vector ================================================|
+# explore how it works
+str_extract_part2 <- function(string, before, pattern) {
+  if (missing(pattern)) {
+    stop("argument `pattern` is missing ")
+  }
+  esc_punt <- c("?", "$", "(", ")", "+", ".", "^", "*", "|", "[", "_", "s", "\\")
+
+  if (any(pattern %in% esc_punt)) {
+    pt <- pattern[pattern %in% esc_punt]
+    pt <- str_glue("\\{pt}")
+    pattern <- c(pattern[!pattern %in% esc_punt], pt)
+  }
+
+  tryCatch(
+    expr = {
+      if (isTRUE(before)) {
+        ext_list <- lapply(pattern, function(.x) {
+          stringr::str_extract(string = string,
+                               pattern = stringr::str_glue("^.*(?={.x})"))
+        })
+        ext_list <- purrr::transpose(ext_list)
+
+        purrr::map_chr(seq_len(length(string)), function(.x) {
+          ext_c <- unlist(ext_list[[.x]])
+          ext_c[!is.na(ext_c)][1]
+        })
+
+      } else if (isFALSE(before)) {
+        ext_list <- lapply(pattern, function(.x) {
+          stringr::str_extract(string = string,
+                               pattern = stringr::str_glue("(?<={.x}).*$"))
+        })
+        ext_list <- purrr::transpose(ext_list)
+
+        purrr::map_chr(seq_len(length(string)), function(.x) {
+          ext_c <- unlist(ext_list[[.x]])
+          ext_c[!is.na(ext_c)][1]
+        })
+      }
+    },
+
+    # needs a better error message ------------------------------- /!\
+    error = function(e) {
+      p_message <- ifelse(length(pattern) == 1,
+                          pattern,
+                          paste(pattern, collapse = ", "))
+
+      stop(stringr::str_glue("can't handle {p_message} pattern"))
+    }
+  )
+}
+
+# Run Example
+# c("1_Monday", "2!Tuesday", "3%Wednesday", "4!Thursday", "5_Friday", "6_Saturday",
+#   "7%Sunday") |>
+#   str_extract_part2(FALSE, c("_", "!", "%"))
+
+
+
